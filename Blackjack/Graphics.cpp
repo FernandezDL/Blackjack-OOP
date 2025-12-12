1c0da65b5a3250e5e8c84e0f1a4547dcdceddffc
Diana Fernández <77743616+FernandezDL@users.noreply.github.com>
2025-12-11 16:16:30 -0800

    Add: Print card visuals
    print Front and back card functions

--- Blackjack/Graphics.cpp
+++ Blackjack/Graphics.cpp
@@ -1,1 +1,31 @@
 #include "Graphics.h"
+#include "Utils.h"
+#include <string>
+
+void Graphics::PrintCardBack(std::ostream& os) const
+{
+    os << "---------\n";
+    os << "|#######|\n";
+    os << "|#######|\n";
+    os << "|#######|\n";
+    os << "|#######|\n";
+    os << "|#######|\n";
+    os << "---------\n";
+}
+
+void Graphics::PrintCardFace(std::ostream& os, int value) const
+{
+    std::string v = std::to_string(value);
+
+    int innerWidth = 7; // espacio entre |       |
+    int left = (innerWidth - (int)v.size()) / 2;
+    int right = innerWidth - (int)v.size() - left;
+
+    os << "+-------+\n";
+    os << "|       |\n";
+    os << "|       |\n";
+    os << "|" << std::string(left, ' ') << v << std::string(right, ' ') << "|\n";
+    os << "|       |\n";
+    os << "|       |\n";
+    os << "+-------+\n";
+}
--- Blackjack/Graphics.h
+++ Blackjack/Graphics.h
@@ -7,5 +7,7 @@ class Graphics
 public:
 	//subscribeCardDrawn(CardDrawnCallback callback) {}
 	//notifyCardDrawn(const Card& card, const std::string& owner) {}
+	void PrintCardBack(std::ostream& os) const;
+	void PrintCardFace(std::ostream& os, int value) const;
 };
 
