9091dc84bdbc678145c189a6bb48c74f74dfad8a
Julian Rosero Narvaez <pg29julian@vfs.com>
2025-12-11 16:50:54 -0800

    Implemented: implemented core game loop, added functions to Game class

--- Blackjack/main.cpp
+++ Blackjack/main.cpp
@@ -1,34 +1,95 @@
 #include "Utils.h"
 #include "Game.h"
+#include <functional>
 
 int main() 
 {
 	// Initialize Game
 	Game game = Game();
-	const int gameWinCondition = 21;
+	const int gameEndCondition = 21;
 	const int initialCardsPerParticipant = 2;
+
+	// Subscriptions
+
+	// TODO: Add subscriptions here
+
+	// Game variables
+	bool deal = false;
+	int counter = 0;
+	bool isGameOver = false;
+	bool isDealerWin = false;
 
 	game.Start();
 	LOG_LN("Starting game!");
 
-	int counter = 0;
-	while (counter < initialCardsPerParticipant)
-	{
-		LOG_LN("Dealing!");
-		game.DealToPlayer();
-		game.DealToDealer();
-
-		counter++;
-	}
-
-	LOG_LN("Finished first round of dealing");
-
 	// Game Loop
-	/*while (true) 
+	while (true) 
 	{
-	}*/
+		// First round of dealing
+		while (counter < initialCardsPerParticipant)
+		{
+			game.DealToPlayer();
+			// TODO: Print Player's Card's value
 
-	
+			game.DealToDealer();
+			// TODO: Print first Dealer's Card
+			counter++;
+		}
+
+		// Player's turn
+		game.UpdateCurrentTurn(CurrentTurn::Input);
+
+		STRING continueInput;
+		do
+		{
+			isGameOver = game.CheckLoseCondition(gameEndCondition);
+
+			CONTINUE_INPUT(continueInput);
+			if (continueInput == "y")
+			{
+				game.DealToPlayer();
+				//TODO: Print total Player's Hand value
+			}
+		} while (continueInput != "n" || !isGameOver);
+
+		if (!isGameOver)
+		{
+			// Dealer's turn
+			game.UpdateCurrentTurn(CurrentTurn::AI);
+			do
+			{
+				isDealerWin = game.CheckDealerWin(gameEndCondition);
+
+				game.DealToDealer();
+				// TODO: Print total Dealer's Hand value
+			} while (!game.CheckLoseCondition(gameEndCondition) || isDealerWin);
+
+			if (isDealerWin)
+			{
+				// TODO: Print player lose
+			}
+			else
+			{
+				// TODO: Print player win
+			}
+		}
+		else
+		{
+			// TODO: Print player lose
+		}
+
+		STRING playAgainInput;
+		PLAYAGAIN_INPUT(playAgainInput);
+
+		if (playAgainInput == "n") break;
+		else 
+		{
+			counter = 0;
+			deal = false;
+			isGameOver = false;
+			isDealerWin = false;
+		}
+	}
 
 	return 0;
 }
